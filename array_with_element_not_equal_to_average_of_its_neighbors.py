2df415194d6ed7fa07ecdc662febef41d663f5ee
abrhamgg <abrhamgg@gmail.com>
2022-12-28 13:43:50 +0300

    medium

---
+++ array_with_element_not_equal_to_average_of_its_neighbors.py
@@ -0,0 +1,11 @@
+class Solution(object):
+
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        for i in range(1, len(nums), 2):
+            nums[i], nums[i - 1] = nums[i - 1], nums[i]
+        return nums
