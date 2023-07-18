0a6b593420ab4793e1a25d67f35aca34ccecbe37
abrhamgg <abrhamgg@gmail.com>
2023-07-18 05:44:46 -0400

    mylist

---
+++ mylist.py
@@ -0,0 +1,31 @@
+if __name__ == '__main__':
+    # Initialize an empty list
+    my_list = []
+
+    # Read the number of commands
+    n = int(input())
+
+    # Perform the commands
+    for _ in range(n):
+        command = input().split()
+
+        # Execute the corresponding command
+        if command[0] == 'insert':
+            index = int(command[1])
+            element = int(command[2])
+            my_list.insert(index, element)
+        elif command[0] == 'print':
+            print(my_list)
+        elif command[0] == 'remove':
+            element = int(command[1])
+            my_list.remove(element)
+        elif command[0] == 'append':
+            element = int(command[1])
+            my_list.append(element)
+        elif command[0] == 'sort':
+            my_list.sort()
+        elif command[0] == 'pop':
+            my_list.pop()
+        elif command[0] == 'reverse':
+            my_list.reverse()
+
