77e5c37e9e6c4e7eeded0b0abad4cb0b6fe6decf
abrhamgg <abrhamgg@gmail.com>
2023-07-18 06:12:31 -0400

    solved in 20 minutes

---
+++ textWrap.py
@@ -0,0 +1,7 @@
+
+
+def wrap(string, max_width):
+    wrapped_string = ''
+    for i in range(0, len(string), max_width):
+        wrapped_string += string[i:i+max_width]+ '\n'
+    return wrapped_string
