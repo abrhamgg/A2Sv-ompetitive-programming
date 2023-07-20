d9f838ee91f795c92dd96c67f2f638a5c8207e18
abrhamgg <abrhamgg@gmail.com>
2023-07-20 08:52:57 -0400

    solved in 8 minutes

---
+++ multiply_strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(eval(num1) * eval(num2))
